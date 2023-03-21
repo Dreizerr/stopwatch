79f3e7163aa1d4deed65652caee947af07c6c390
Dreizer <artem39r@mail.ru>
2023-03-21 20:00:13 +0200

    костыль

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
     roundTimerScoreSeconds = document.querySelector(".stopwatch__round-seconds"),
     roundBody = document.querySelector(".stopwatch__round-body");
 
-  let countTime = 0,
+  let countTime = 3597,
     roundCountTime = 0,
     stopwatchStarted = false,
     stashOrder = 1;
@@ -20,11 +20,8 @@ document.addEventListener("DOMContentLoaded", function () {
   playButton.addEventListener("click", playToggle);
 
   let countInterval = setInterval(timeCount, 1000);
-  // roundCountInterval = setInterval(roundTimeCount, 1000);
 
   clearInterval(countInterval);
-
-  // clearInterval(roundCountInterval);
 
   function playToggle() {
     if (!stopwatchStarted) {
@@ -38,11 +35,9 @@ document.addEventListener("DOMContentLoaded", function () {
         timeCount();
         roundTimeCount();
       }, 1000);
-      // roundCountInterval = setInterval(, 1000);
     } else {
       replayButton.classList.add("active");
       clearInterval(countInterval);
-      // clearInterval(roundCountInterval);
     }
     stopwatchStarted = true;
   }
@@ -51,13 +46,38 @@ document.addEventListener("DOMContentLoaded", function () {
     if (clear === 0) {
       countTime = 0;
     }
+
     let seconds = Math.floor(countTime % 60);
     let minutes = Math.floor((countTime / 60) % 60);
+
     seconds = seconds < 10 ? `0${seconds}` : seconds;
     minutes = minutes < 10 ? `0${minutes}` : minutes;
+
     timerScoreSeconds.textContent = seconds;
     timerScoreMinutes.textContent = minutes;
+
+    if (minutes === 59 && seconds === 59) {
+      replayFunc(false);
+      countTime = 0;
+      roundCountTime = 0;
+    }
+
+    console.log(countTime, "MAIN");
     countTime++;
+  }
+
+  function roundTimeCount(clear) {
+    if (clear === 0) {
+      roundCountTime = 0;
+    }
+    let seconds = Math.floor(roundCountTime % 60);
+    let minutes = Math.floor((roundCountTime / 60) % 60);
+    seconds = seconds < 10 ? `0${seconds}` : seconds;
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+    roundTimerScoreSeconds.textContent = seconds;
+    roundTimerScoreMinutes.textContent = minutes;
+    console.log(roundCountTime, "NMAIN");
+    roundCountTime++;
   }
 
   timeCount();
@@ -70,22 +90,41 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function replayFunc(event) {
     if (replayButton.classList.contains("active")) {
-      roundBody.style.opacity = "0";
+      // roundBody.style.opacity = "0";
+      timeCount(0);
+      roundTimeCount(0);
+      clearInterval(countInterval);
+
       const stashItems = document.querySelectorAll(".stopwatch-stash__item");
       stashItems.forEach((elem) => {
         elem.remove();
       });
-      timeCount(0);
-      roundTimeCount(0);
-      clearInterval(countInterval);
 
       replayButton.classList.remove("active");
       stopwatchStarted = false;
       roundStart = false;
 
       stashOrder = 1;
+    } else if (event === false) {
+      // roundBody.style.opacity = "0";
+
+      timeCount(0);
+      roundTimeCount(0);
+
+      clearInterval(countInterval);
+      const stashItems = document.querySelectorAll(".stopwatch-stash__item");
+      stashItems.forEach((elem) => {
+        elem.remove();
+      });
+
+      replayButton.classList.remove("active");
+      stopwatchStarted = false;
+      roundStart = false;
+
+      stashOrder = 1;
+      stopwatchStateToggler();
     } else {
-      event.preventDefault();
+      if (event) event.preventDefault();
     }
   }
 
@@ -106,26 +145,13 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  function roundTimeCount(clear) {
-    if (clear === 0) {
-      roundCountTime = 0;
-    }
-    let seconds = Math.floor(roundCountTime % 60);
-    let minutes = Math.floor((roundCountTime / 60) % 60);
-    seconds = seconds < 10 ? `0${seconds}` : seconds;
-    minutes = minutes < 10 ? `0${minutes}` : minutes;
-    roundTimerScoreSeconds.textContent = seconds;
-    roundTimerScoreMinutes.textContent = minutes;
-    roundCountTime++;
-  }
-
   function stopwatchStateToggler() {
     stopwatch.classList.toggle("active");
     ButtonsStateToggler();
   }
 
   function replayAddListener() {
-    replayButton.addEventListener("click", replayFunc, { once: true });
+    replayButton.addEventListener("click", replayFunc);
   }
 
   function roundAddListener() {
